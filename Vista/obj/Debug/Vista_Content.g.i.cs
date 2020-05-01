6e8c0107366217ef814723029f7dc5960e95b38b
JElizabeth1988 <johana.elizabeth1988@gmail.com>
2020-05-01 01:36:19 -0400

    :)

--- Vista/obj/Debug/Vista_Content.g.i.cs
+++ Vista/obj/Debug/Vista_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("imagenes/logo11.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("imagenes/logo%20yo.jpg")]
 
 
